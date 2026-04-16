d3c49bf0c728fe965e10c090572aff9fb56875c6
Jasmine Mathisen <mathisee093006@gmail.com>
2026-04-16 14:09:07 -0700

    fix CI: add setImmediate polyfill

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,3 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+
 import '@testing-library/jest-dom';
